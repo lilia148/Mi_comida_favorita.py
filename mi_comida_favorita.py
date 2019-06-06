cf18d45ce8148b60e85c631eb4b47d3f1c6545f9
lilia148 <51103968+lilia148@users.noreply.github.com>
2019-06-05 20:25:33 -0500

    Add files via upload

---
+++ mi_comida_favorita.py
@@ -0,0 +1,41 @@
+comidas = ["Pepperoni pizza = pizza de pepperoni."
+    "Hot dog = perro caliente."
+    "Hamburger = hamburguesa."
+    "Mac n' cheese = macarrones con queso."
+    "Peanut butter and jelly sandwich = sándwich de mantequilla de maní y jalea."
+    "Buffalo wings = alitas de búfalo."
+    "Coleslaw = ensalada de repollo."
+	"Barbecue ribs = costillas de cerdo en salsa barbacoa"
+	"galletas con chispas de chocolate"
+	"brownie"
+	"Puré de papas"
+	"pescado"
+	"tacos M"
+	"ensalada"
+	"Sándwich",
+	"Papas fritas", 
+	"Papas fritas inglesas", 
+	"salmon"
+	"San jacobo"
+	Pechugas Cordon Blue"]
+
+def imprimir menu()
+	print("escoge tus 5 comidas favoritas :")
+	for comida in comidas 
+		print(comida)
+		
+imprimir_menu()		
+
+print()
+comidas favoritas = [Hot dog = perro caliente."
+    "Hamburger = hamburguesa."
+    "Mac n' cheese", "galletas con chispas de chocolate"
+	"Papas fritas"]
+	
+	def imprimir menu()
+	print("estan son mis 5 comidas favoritas :")
+	for comida in comidas 
+		print(comida)
+		
+imprimir_menu()		
+	
